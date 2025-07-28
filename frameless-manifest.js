e65e81d937dd803567a4c2db16cda669418df68c
maeiky <maeiky@gmail.com>
2025-07-27 21:13:21 -0400

    #

--- frameless-manifest.js
+++ frameless-manifest.js
@@ -1,27 +1,122 @@
 window.FRAMELESS_MANIFEST = [
   {
+    image: "https://wkweb.github.io/wkspell/images/showcase/multi-windows.png",
+    title: "Multi-Windows Rendering",
+    description: "Multi-Windows rendering by Wake"
+  },
+
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/transparency.gif",
+    title: "Transparency",
+    description: "Wake support semi-transparency composition directly from the GPU"
+  },
+  
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/shadertoy01.gif",
+    title: "Shadertoy",
+    description: "Create Shadetoy GLSL directly in your native environement"
+  },
+  
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/self-editor.png",
+    title: "Bootstrap",
+    description: "This window displays the editor we use to edit itself. Occasionally, singularities emerge within our engine"
+  },
+  
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/3dArt01.gif",
+    title: "3d art",
+    description: "Art experiment on Wake"
+  },
+  
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/font.gif",
+    title: "Font",
+    description: "This is a dynamic GPU font, JIT with Wake"
+  },
+
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/implot.png",
+    title: "Plot",
+    description: "Render interactive plots on the fly" 
+  },
+  
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/playground01.gif",
+    title: "JIT playground",
+    description: "A JIT plaground on Wake"
+  },
+  
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/playground02.png",
+    title: "JIT playground",
+    description: "A JIT plaground on Wake"
+  },
+  
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/playground03.png",
+    title: "JIT playground",
+    description: "A JIT plaground on Wake"
+  },
+  
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/normals02.png",
+    title: "Normals",
+    description: "Playing with normals on Wake"
+  },
+  
+  {
     image: "https://wkweb.github.io/wkspell/images/showcase/3dArt02.png",
-    title: "Wake in Action 1",
-    description: "Clavier repositionnable avec suivi en temps réel."
+    title: "3d art",
+    description: "Art experiment on Wake"
   },
+
   {
-    image: "images/frameless/02.png",
-    title: "Wake in Action 2",
-    description: "Assignation dynamique des régions tactiles."
+    image: "https://wkweb.github.io/wkspell/images/showcase/3dArt04.png",
+    title: "3d art",
+    description: "Art experiment on Wake"
   },
+
   {
-    image: "images/frameless/03.png",
-    title: "Wake in Action 3",
-    description: "Surface interactive libre, sans contrainte de layout."
+    image: "https://wkweb.github.io/wkspell/images/showcase/lightballs.gif",
+    title: "Lights",
+    description: "This is a rendering of a perfectly spherical sphere, illuminated by multiple lights"
   },
+
   {
-    image: "images/frameless/04.gif",
-    title: "Wake in Action 4",
-    description: "Support multitouch + drag + gestures complexes."
+    image: "https://wkweb.github.io/wkspell/images/showcase/face_mesh.gif",
+    title: "Media Pipe",
+    description: "Create stunning AI native app with Wake"
   },
+  
   {
-    image: "images/frameless/05.gif",
+    image: "https://wkweb.github.io/wkspell/images/showcase/pi.gif",
+    title: "3d art",
+    description: "Mathematical PI Art experiment on Wake"
+  },
+
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/muti-render01.png",
+    title: "Multiple Render Targets",
+    description: "Combine colors, depth, nomrals, in one GPU pass"
+  },
+ 
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/3dArt02.png",
+    title: "Wake in Action 5",
+    description: "Mode démonstration haute fluidité en boucle."
+  },
+  
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/muti-render02.png",
+    title: "Multiple Render Targets",
+    description: "Combine colors, depth, nomrals, in one GPU pass"
+  },
+
+  {
+    image: "https://wkweb.github.io/wkspell/images/showcase/3dArt02.png",
     title: "Wake in Action 5",
     description: "Mode démonstration haute fluidité en boucle."
   }
+  
 ];
